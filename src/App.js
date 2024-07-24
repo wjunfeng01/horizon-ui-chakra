0e5c9736709775eb22d67dc8c44d1f9ae3184aff
simmmpleweb <hello@simmmple.com>
2024-07-24 23:53:17 +0300

    Removed hypertheme

--- src/App.js
+++ src/App.js
@@ -1,39 +1,38 @@
 import './assets/css/App.css';
 import { Routes, Route, Navigate } from 'react-router-dom';
+import {} from 'react-router-dom';
 import AuthLayout from './layouts/auth';
 import AdminLayout from './layouts/admin';
 import RTLLayout from './layouts/rtl';
-import React from 'react';
-import theme from 'theme/theme';
-import { ThemeEditorProvider } from '@hypertheme-editor/chakra-ui';
-import { ChakraProvider } from '@chakra-ui/react';
+import {
+  ChakraProvider,
+  // extendTheme
+} from '@chakra-ui/react';
 import initialTheme from './theme/theme'; //  { themeGreen }
 import { useState } from 'react';
+// Chakra imports
 
 export default function Main() {
+  // eslint-disable-next-line
   const [currentTheme, setCurrentTheme] = useState(initialTheme);
   return (
-    <ChakraProvider theme={theme}>
-      <React.StrictMode>
-        <ThemeEditorProvider>
-          <Routes>
-            <Route path="auth/*" element={<AuthLayout />} />
-            <Route
-              path="admin/*"
-              element={
-                <AdminLayout theme={currentTheme} setTheme={setCurrentTheme} />
-              }
-            />
-            <Route
-              path="rtl/*"
-              element={
-                <RTLLayout theme={currentTheme} setTheme={setCurrentTheme} />
-              }
-            />
-            <Route path="/" element={<Navigate to="/admin" replace />} />
-          </Routes>
-        </ThemeEditorProvider>
-      </React.StrictMode>
+    <ChakraProvider theme={currentTheme}>
+      <Routes>
+        <Route path="auth/*" element={<AuthLayout />} />
+        <Route
+          path="admin/*"
+          element={
+            <AdminLayout theme={currentTheme} setTheme={setCurrentTheme} />
+          }
+        />
+        <Route
+          path="rtl/*"
+          element={
+            <RTLLayout theme={currentTheme} setTheme={setCurrentTheme} />
+          }
+        />
+        <Route path="/" element={<Navigate to="/admin" replace />} />
+      </Routes>
     </ChakraProvider>
   );
 }
